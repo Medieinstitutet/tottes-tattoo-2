9a4cdc65eaad58859247df29b2fcb11941601a44
Rasta Dave <david.borg.kevin321@gmail.com>
2025-05-14 16:27:11 +0200

    Lägg till Artist-modell, controller och routes

--- backend/src/app.mjs
+++ backend/src/app.mjs
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDb } from './db/db.mjs';
+import artistRouter from './routes/artistRoutes.mjs';
 
 dotenv.config();
 
@@ -9,5 +10,7 @@ const app = express();
 app.use(express.json());
 
 connectDb();
+
+app.use('/api/v1/artists', artistRouter);
 
 export { app };
