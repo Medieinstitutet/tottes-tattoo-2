21c5dede71370f11b735b853a7492cc598ff1169
Gl373 <jihiglete@yahoo.com>
2025-05-15 17:58:46 +0200

    :file_folder: Reorganize folder structure: move files to src-folder

--- backend/src/app.mjs
+++ backend/src/app.mjs
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static('../uploads'));
 app.use('/bookings', bookingRoutes);
 app.use(errorHandler); // Alltid sista
 
