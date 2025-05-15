44fbfa402d8316d3cd726e64e5d2e4456259d756
rainpraats <rainpraats@gmail.com>
2025-05-15 16:38:56 +0200

    added controller for bookings

--- backend/controllers/booking-controller.mjs
+++ backend/controllers/booking-controller.mjs
@@ -1,1 +1,27 @@
 import BookingRepository from '../repositories/bookingRepository.mjs';
+import { catchErrorAsync } from '../utilities/catchErrorAsync.mjs';
+
+export const listAllBookings = catchErrorAsync(async (req, res) => {
+  const bookings = await new BookingRepository().listAll();
+  res.status(200).json({ success: true, data: bookings });
+});
+
+export const findBookingById = catchErrorAsync(async (req, res) => {
+  const booking = await new BookingRepository.findById(req.params.id);
+  res.status(200).json({ success: true, data: booking });
+});
+
+export const addBooking = catchErrorAsync(async (req, res) => {
+  const booking = await new BookingRepository.add(req.body);
+  res.status(201).json({ success: true, data: booking });
+});
+
+export const updateBooking = catchErrorAsync(async (req, res) => {
+  const booking = await new BookingRepository.update(req.body);
+  res.status(201).json({ success: true, data: booking });
+});
+
+export const removeBooking = catchErrorAsync(async (req, res) => {
+  await new BookingRepository.remove(req.params.id);
+  res.status(204);
+});
