c49cac54dc36c52e50336dd45492cbeb5fba9072
ritawi8 <rita.wildrot@gmail.com>
2025-05-16 18:40:19 +0200

    adminpanel -matcha rubrikens stil med start sidan

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,18 +4,29 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from './Pages/theme';
 import StartPage from './Pages/start-page.jsx';
 import BookingPage from './Pages/booking-page.jsx';
+import AdminPage from './Pages/admin-page.jsx';
 
 const App = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <Routes>
-          <Route path='/' element={<StartPage />} />
-          <Route path='/booking' element={<BookingPage />} />
-        </Routes>
-      </Router>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<Router>
+				<Routes>
+					<Route
+						path='/'
+						element={<StartPage />}
+					/>
+					<Route
+						path='/booking'
+						element={<BookingPage />}
+					/>
+					<Route
+						path='/admin'
+						element={<AdminPage />}
+					/>
+				</Routes>
+			</Router>
+		</ThemeProvider>
+	);
 };
 
 export default App;
