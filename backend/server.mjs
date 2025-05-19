ae29e23a9e6b6040ac6e7aa3ce96fc369f78b788
lotizsj <your.email@example.com>
2025-05-19 17:50:05 +0200

    Lös merge-konflikt i backend/server.mjs

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,14 +1,12 @@
-<<<<<<< HEAD
 import dotenv from 'dotenv';
 dotenv.config();
 
-import app from './app.mjs';
-=======
 import { app } from './app.mjs';
->>>>>>> booking-page
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT} (${process.env.NODE_ENV})`);
+  console.log(
+    `Server running at http://localhost:${PORT} (${process.env.NODE_ENV})`
+  );
 });
